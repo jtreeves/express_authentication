cec994a9ae7323701b8facda379f4861d5f0a0a8
jtreeves <jackson.t.reeves@gmail.com>
2020-11-23 14:34:30 -0500

    [ppConfig] Add local strategy

--- config/ppConfig.js
+++ config/ppConfig.js
@@ -19,3 +19,20 @@ passport.deserializeUser((id, cb) => {
         console.log(err)
     })
 })
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, (email, password, cb) => {
+    db.user.findOne({
+        where: { email }
+    }).then(user => {
+        if (!user || !user.validPassword(password)) {
+            cb(null, false)
+        } else {
+            cb(null, user)
+        }
+    }).catch(cb)
+}))
+
+module.exports = passport
