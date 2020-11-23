d656c70c0eaa0ca572571fa8c2c2d646b97e53f3
jtreeves <jackson.t.reeves@gmail.com>
2020-11-23 12:50:46 -0500

    [server] Create sessionObject for express-sessions

--- server.js
+++ server.js
@@ -1,29 +1,44 @@
-require('dotenv').config();
-const express = require('express');
-const layouts = require('express-ejs-layouts');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const layouts = require('express-ejs-layouts')
+const session = require('express-session')
+const passport = require('./config/ppConfig')
+const flash = require('connect-flash')
+const SECRET_SESSION = process.env.SECRET_SESSION
+const app = express()
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
-app.use(require('morgan')('dev'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
-app.use(layouts);
+app.use(require('morgan')('dev'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/public'))
+app.use(layouts)
+
+const sessionObject = {
+    // What we actually will be giving the user on our site as a session cookie
+    secret: SECRET_SESSION,
+    // Save the session, even if it's modified (so we make it false)
+    resave: false,
+    // If we have a new session, we save it (so we make it true)
+    saveUninitialized: true
+}
+
+app.use(session(sessionObject))
 
 app.get('/', (req, res) => {
-  res.render('index');
-});
+    res.render('index')
+})
 
 app.get('/profile', (req, res) => {
-  res.render('profile');
-});
+    res.render('profile')
+})
 
-app.use('/auth', require('./routes/auth'));
+app.use('/auth', require('./routes/auth'))
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 const server = app.listen(PORT, () => {
-  console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
-});
+    console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`)
+})
 
-module.exports = server;
+module.exports = server
